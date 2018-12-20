35152d6c70b001b8940b534fc34f364f93049341
mekaylayoung <44011243+mekaylayoung@users.noreply.github.com>
2018-12-20 00:44:42 -0500

    Add files via upload

---
+++ lesson7/problem3.py
@@ -0,0 +1,18 @@
+print('-' * 65)
+print('Year when you turn 100')
+print()
+
+print('Description: This will tell you what year you will turn 100.')
+print()
+
+current = input('Current year: ')
+current = int(current)
+age = input('How old are you?: ')
+age = int(age)
+born = current - age
+future = born + 100
+future = str(future)
+
+print('You will turn 100 in the year ' + future)
+print()
+pring('-' * 65)
